48986c8c2a04eee7191a0b9b51ecefae2cefc069
Devan <iskandardevanto@telkomuniversity.ac.id>
2023-09-18 13:36:43 +0700

    first commit

---
+++ src/data/menu.js
@@ -0,0 +1,177 @@
+export var menuItems={
+  "data": [
+    {
+      "headTitle1": "Pages",
+      "headTitle2": "All Neccesory Pages Added",
+      "type": "headtitle"
+    },
+    {
+      "path": "/pages/sample-page",
+      "title": "Sample page",
+      "icon": "stroke-sample-page",
+      "iconf":"fill-sample-page",
+      "type": "link",
+      "active":false
+    },
+    {
+      "title": "Error Page",
+      "icon": "stroke-internationalization",
+      "iconf":"fill-internationalization",
+      "type": "sub",
+      "active": false,
+      "children": [
+        {
+          "path": "/error-400",
+          "title": "Error 400",
+          "type": "link",
+          "active":false
+        },
+        {
+          "path": "/error-401",
+          "title": "Error 401",
+          "type": "link",
+          "active":false
+        },
+        {
+          "path": "/error-403",
+          "title": "Error 403",
+          "type": "link",
+          "active":false
+        },
+        {
+          "path": "/error-404",
+          "title": "Error 404",
+          "type": "link",
+          "active":false
+        },
+        {
+          "path": "/error-500",
+          "title": "Error 500",
+          "type": "link",
+          "active":false
+        },
+        {
+          "path": "/error-503",
+          "title": "Error 503",
+          "type": "link",
+          "active":false
+        }
+      ]
+    },
+    {
+      "title": "Authentication",
+      "icon": "stroke-internationalization",
+      "iconf":"fill-internationalization",
+      "type": "sub",
+      "active": false,
+      "children": [
+        {
+          "path": "/auth/login",
+          "title": "Login Simple",
+          "type": "link",
+          "active":false
+        },
+        {
+          "path": "/authentication/login/one",
+          "title": "Login With Image",
+          "type": "link",
+          "active":false
+        },
+        {
+          "path": "/authentication/login/two",
+          "title": "Login With Image 2",
+          "type": "link",
+          "active":false
+        },
+        {
+          "path": "/authentication/login/validate",
+          "title": "Login With Validation",
+          "type": "link",
+          "active":false
+        },
+        {
+          "path": "/authentication/login/tooltip",
+          "title": "Login With Tooltip",
+          "type": "link",
+          "active":false
+        },
+        {
+          "path": "/authentication/login/sweetalert",
+          "title": "Login With Sweetalert",
+          "type": "link",
+          "active":false
+        },
+        {
+          "path": "/auth/register",
+          "title": "Register Simple",
+          "type": "link",
+          "active":false
+        },
+        {
+          "path": "/authentication/register/image",
+          "title": "Register with Image",
+          "type": "link",
+          "active":false
+        },
+        {
+          "path": "/authentication/register/image2",
+          "title": "Register with image 2",
+          "type": "link",
+          "active":false
+        },
+  
+        {
+          "path": "/authentication/unlockuser",
+          "title": "Unlock User",
+          "type": "link",
+          "active":false
+        },
+        {
+          "path": "/authentication/forgetpassword",
+          "title": "Forget Password",
+          "type": "link",
+          "active":false
+        },
+        {
+          "path": "/authentication/resetpassword",
+          "title": "Reset Password",
+          "type": "link",
+          "active":false
+        },
+        {
+          "path": "/authentication/maintenance",
+          "title": "Maintenance",
+          "type": "link",
+          "active":false
+        }
+      ]
+    },
+    {
+      "title": "Coming Soon",
+      "icon": "stroke-internationalization",
+      "iconf":"fill-internationalization",
+      "type": "sub",
+      "active": false,
+      "children": [
+        {
+          "path": "/comingsoon/comingsoon-simple",
+          "title": "Coming Simple",
+          "type": "link",
+          "active":false
+        },
+        {
+          "path": "/comingsoon/comingsoon-image",
+          "title": "Coming with Bg Image",
+          "type": "link",
+          "active":false
+        },
+        {
+          "path": "/comingsoon/comingsoon-video",
+          "title": "Coming with Bg video",
+          "type": "link",
+          "active":false
+        }
+      ]
+    },
+  ]
+}
